1246f492ce5cc39a01af64647a44cef2ec6e5075
林博仁(Buo-ren Lin) <Buo.Ren.Lin@gmail.com>
2018-12-28 00:52:33 +0800

    Switch coding style to spaces
    Signed-off-by: 林博仁(Buo-ren Lin) <Buo.Ren.Lin@gmail.com>

--- snap/local/build-utilities/set-snap-version.bash
+++ snap/local/build-utilities/set-snap-version.bash
@@ -3,45 +3,45 @@
 # 林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com> © 2018
 
 set \
-	-o errexit \
-	-o errtrace \
-	-o nounset \
-	-o pipefail
+    -o errexit \
+    -o errtrace \
+    -o nounset \
+    -o pipefail
 
 init(){
-	local \
-		upstream_version \
-		packaging_revision
+    local \
+        upstream_version \
+        packaging_revision
 
-	upstream_version="$(
-		# FIXME: Should be more robust
-		grep \
-			'^AC_INIT(' \
-			<parts/filezilla/src/configure.ac \
-		| cut \
-			--characters=9- \
-		| cut \
-			--delimiter=, \
-			--fields=2 \
-		| sed \
-			's/^.\(.*\).$/\1/'
-	)"
+    upstream_version="$(
+        # FIXME: Should be more robust
+        grep \
+            '^AC_INIT(' \
+            <parts/filezilla/src/configure.ac \
+        | cut \
+            --characters=9- \
+        | cut \
+            --delimiter=, \
+            --fields=2 \
+        | sed \
+            's/^.\(.*\).$/\1/'
+    )"
 
-	packaging_revision="$(
-		git \
-			describe \
-			--abbrev=4 \
-			--always \
-			--match nothing \
-			--dirty=-d
-	)"
+    packaging_revision="$(
+        git \
+            describe \
+            --abbrev=4 \
+            --always \
+            --match nothing \
+            --dirty=-d
+    )"
 
-	printf \
-		-- \
-		'%s' \
-		"${upstream_version}+pkg-${packaging_revision}"
+    printf \
+        -- \
+        '%s' \
+        "${upstream_version}+pkg-${packaging_revision}"
 
-	exit 0
+    exit 0
 }
 
 init "${@}"
